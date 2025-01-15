0728fe6e413005ab0194b1649ec7928d49ba9b34
LM1Training <149112316+LM1Training@users.noreply.github.com>
2025-01-15 13:04:25 -0500

    New Format Update

---
+++ prime2/files/search/book_config.js
@@ -0,0 +1,1 @@
+var textForPages =["Bookmarks\u000d\u000aCompany Policies & \u000d\u000aEmployee Resources\u000d\u000aMetros \u000d\u000aSummary","Bookmarks\u000d\u000aCompany Policies & \u000d\u000aEmployee Resources\u000d\u000aMetros \u000d\u000aSummary"]
